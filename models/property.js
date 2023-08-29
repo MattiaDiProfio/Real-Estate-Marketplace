6ae73c064c3c4f2f00d54b593f980d668694ecd9
Mattia Di Profio <mattia.diprofio@gmail.com>
2023-08-29 11:43:18 +0100

    set up express app and connection to mongoDB

---
+++ models/property.js
@@ -0,0 +1,15 @@
+/**
+ * Mongoose model to represent a Property Entity in the Mongo DB
+ */
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const PropertySchema = new Schema({
+    address : String,
+    description : String,
+    monthlyRent : Number,
+    numberRooms : Number
+});
+
+module.exports = new mongoose.model('Property', PropertySchema);
