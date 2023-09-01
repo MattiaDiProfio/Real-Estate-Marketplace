172c4f36c58b92756e237d6e87e58f1dff681383
Mattia Di Profio <mattia.diprofio@gmail.com>
2023-09-01 10:34:31 +0100

    add basic property CRUD functionality

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const Property = require('./models/property');
+const property = require('./models/property');
 
 //set up connection to mongoDB
 mongoose.connect('mongodb://localhost:27017/PropertEase', { useNewUrlParser : true });
@@ -12,9 +16,60 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended : true }));
+app.use(methodOverride('_method'));
+
 
 app.get('/', async (request, response) => {
     response.render('home')
 });
+
+// render a view of all properties
+app.get('/properties', async (req, res) => {
+    const allProperties = await Property.find({});
+    res.render('properties/index', { allProperties });
+});
+
+//serve the form to create new property
+app.get('/properties/new', (req, res) => {
+    res.render('properties/new');
+})
+
+// render a more detailed view of a single property
+app.get('/properties/:id', async(req, res) => {
+    const { id } = req.params;
+    const property = await Property.findById(id);
+    res.render('properties/show', { property });
+})
+
+// serve form to edit a specific rout
+app.get('/properties/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const property = await Property.findById(id);
+    res.render('properties/edit', { property });
+});
+
+
+// post new property info to the db
+app.post('/properties', async (req, res) => {
+    const newProperty = new Property(req.body.property);
+    await newProperty.save();
+    res.redirect(`/properties/${newProperty._id}`);
+});
+
+// edit property using data from the edit form
+app.put('/properties/:id/edit', async(req, res) => {
+    const { id } = req.params;
+    const property = await Property.findByIdAndUpdate(id, { ...req.body.property }, { new : true });
+    await property.save();
+    res.redirect(`/properties/${property._id}`)
+});
+
+// delete property from db
+app.delete('/properties/:id', async(req, res) => {
+    const { id } = req.params;
+    await Property.findByIdAndDelete(id);
+    res.redirect('/properties');
+})
 
 app.listen(3000, () => console.log('server live on port 3000'));
