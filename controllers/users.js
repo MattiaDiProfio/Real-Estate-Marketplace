d3488c5b9c0a76865d7d9f8452118b2830ddb8c0
Mattia Di Profio <mattia.diprofio@gmail.com>
2023-09-07 11:45:19 +0100

    implement Upcoming Viewings section for landlords

--- controllers/users.js
+++ controllers/users.js
@@ -44,7 +44,10 @@ module.exports.logoutUser = (req, res, next) => {
 
 module.exports.showUserDashboard = async (req, res) => {
     const { id } = req.params;
-    const user = await User.findById(id).populate('myListings').populate({path : 'upcomingViewings', populate : { path : 'property' }});
+    const user = await User.findById(id)
+        .populate({path : 'myListings', populate : { path : 'viewings', populate : [{path : 'date'}, {path : 'requestedBy', populate : {path : 'username'}}, {path : 'property'}] }})
+        .populate({path : 'upcomingViewings', populate : { path : 'property' }});
+
     res.render('users/dashboard', { user });
 };
 
